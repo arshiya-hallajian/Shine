6320e7bf506162a05634697f57d52f3b92fc7af6
ArshiyA <arshiyah52@gmail.com>
2023-03-13 00:24:30 +0330

    adds all auth controllers functions

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,17 @@
+
+
+module.exports.login_post = (req, res) = {
+    
+}
+
+module.exports.signup_post = (req, res) = {
+    
+}
+
+module.exports.logout_get = (req, res) = {
+    
+}
+
+module.exports.userlist_get = (req, res) = {
+    
+}

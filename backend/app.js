719600639d6a0f3bc1a60777689a578bcfa91343
ArshiyA <arshiyah52@gmail.com>
2023-03-13 00:52:20 +0330

    fixed auth router variable

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 4001
-const auth.router = require('./routes/auth.router')
+const auth_router = require('./routes/auth.router')
 
 //middleware 
 app.use(cors())
 
 
 //auth router
-app.use("/api/auth", auth.router)
+app.use("/api/auth", auth_router)
 
 
 app.listen(port, () => console.log(`listening on port ${port}!`))

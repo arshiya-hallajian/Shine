14e9323069698cb244572226581d28f957f3106a
ArshiyA <arshiyah52@gmail.com>
2023-03-13 00:51:18 +0330

    added auth route in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 4001
+const auth.router = require('./routes/auth.router')
 
 //middleware 
 app.use(cors())
 
 
+//auth router
+app.use("/api/auth", auth.router)
 
 
 app.listen(port, () => console.log(`listening on port ${port}!`))

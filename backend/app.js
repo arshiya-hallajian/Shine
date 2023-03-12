3b8c3059a878155f867be90f9464e458a3a9dc48
ArshiyA <arshiyah52@gmail.com>
2023-03-13 02:24:53 +0330

    mongoose connect

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,15 @@ const app = express()
 const cors = require('cors')
 const port = 4001
 const auth_router = require('./routes/auth.router')
+const mongoose = require('mongoose')
 
 //middleware 
 app.use(cors());
 
+//database connect 
+mongoose.connect('mongodb+srv://arshiyah53:46646659@cluster0.p7fvwrc.mongodb.net/user?retryWrites=true&w=majority').then(()=>{
+    console.log("connected to db")
+})
 
 //auth router                 
 app.use("/api/auth", auth_router);

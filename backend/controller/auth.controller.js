2bd1454ff34e835dfb55164490420c58f9ef44ea
ArshiyA <arshiyah52@gmail.com>
2023-03-13 22:57:13 +0330

    signup added

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,12 +1,34 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user")
 
 
 module.exports.login = (req, res) => {
     res.send("login")
 }
 
-module.exports.signup = (req, res) => {
-    res.send("signup")
+module.exports.signup = async(req, res) => {
+    // res.send(req.body)
+    const user = new User({
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+        email : req.body.email,
+        password: req.body.password
+    })
+
+    try{
+        const saveUser = await user.save();
+        res.status(200).json({
+            status: true
+        });
+    }catch(e){
+        res.status(403).json({
+            status: false,
+            error: e
+        })
+    }
 }
+
 
 module.exports.logout = (req, res) => {
     res.send("logout")

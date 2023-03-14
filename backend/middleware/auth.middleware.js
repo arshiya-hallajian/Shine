9174c4738ea386cf197799790c82b489ebcbff56
Arshiya <arshiyah52@gmail.com>
2023-03-15 01:37:17 +0330

    check token & verify it

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,3 +1,13 @@
 const jwt = require('jsonwebtoken');
 const Token = require('../models/token');
 
+const auth = (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) return res.status(400).send('access denied');
+
+    const verified = jwt.verify(token, "MySecretKey").then(err => {
+        console.log(err);
+    });
+
+
+}

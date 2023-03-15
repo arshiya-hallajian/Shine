6ed29eb5c962c3c4ca9c22b50e6b98317d6c1662
Arshiya <arshiyah52@gmail.com>
2023-03-15 04:01:01 +0330

    cleaning code

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const Token = require('../models/token');
 
 module.exports = auth = async (req, res, next) => {
+
     //check auth-token header existence
     const token = req.header('auth-token');
     if (!token) return res.status(400).send('access denied');
@@ -11,13 +12,7 @@ module.exports = auth = async (req, res, next) => {
         const verified = jwt.verify(token, "MySecretKey");
 
         //find token in database
-        const userAuth = await Token.findOne(
-            {
-                _userId: verified.id,
-                token: token,
-                tokenType: 'login'
-            }
-        ).exec();
+        const userAuth = await Token.findOne({_userId: verified.id, token: token, tokenType: 'login'}).exec();
 
         //check if user not exist in token collection
         if (!userAuth) {
@@ -28,7 +23,9 @@ module.exports = auth = async (req, res, next) => {
             req.token = token
             next();
         }
-    }catch(e){
+
+
+    } catch (e) {
         //catch error for verified
         res.status(400).send("invalid token");
     }

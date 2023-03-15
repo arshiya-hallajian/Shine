8606a0e8ba011c85ce5fde948131f3ecb969b092
elliot sad <bamdadmahdi05@gmail.com>
2023-03-16 00:55:36 +0100

    add transporter for nodemailer

--- backend/services/sendemail.js
+++ backend/services/sendemail.js
@@ -2,7 +2,21 @@ const nodemailer = require("nodemailer");
 
 // add send email servis
 module.exports = sendMail = async(email, subject , link) => {
-  
+  try{
+    
+    // add transporter for nodemailer
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "94503d5c72dc76",
+    pass: "7ff670a3304fb3"
+  }
+});
 
-  
+
+  }catch(error){
+    console.log("shitty mail error")
+}
+
 }

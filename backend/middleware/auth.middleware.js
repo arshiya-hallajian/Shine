1f599b20d58f3099bb47295f1e48884c85325551
Arshiya <arshiyah52@gmail.com>
2023-03-15 03:33:06 +0330

    fixing from first check token header

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,22 +5,5 @@ module.exports = auth = async (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) return res.status(400).send('access denied');
 
-    jwt.verify(token, "MySecretKey", (err, decoded) => {
-        if(err) return res.status(400).send("invalid token");
-        try {
-            Token.findOne({_userId: decoded.id, token: token, tokenType: 'login'},(err, UserAuthed) => {
-                if(err) return res.status(400).send(err);
-                if(!UserAuthed) {
-                    res.status(400).send(UserAuthed);
-                }else {
-                console.log(UserAuthed);
-                res.send(UserAuthed.token)
-                }
 
-            })
-
-        }catch (e) {
-            res.status(400).send("user")
-        }
-    })
 }

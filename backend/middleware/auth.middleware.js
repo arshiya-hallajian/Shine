ec75f72416f0ae0537734df31f19c60e5a607d5a
Arshiya <arshiyah52@gmail.com>
2023-03-15 03:32:15 +0330

    jwt verify

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,13 +1,26 @@
 const jwt = require('jsonwebtoken');
 const Token = require('../models/token');
 
-const auth = (req, res, next) => {
+module.exports = auth = async (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) return res.status(400).send('access denied');
 
-    const verified = jwt.verify(token, "MySecretKey").then(err => {
-        console.log(err);
-    });
+    jwt.verify(token, "MySecretKey", (err, decoded) => {
+        if(err) return res.status(400).send("invalid token");
+        try {
+            Token.findOne({_userId: decoded.id, token: token, tokenType: 'login'},(err, UserAuthed) => {
+                if(err) return res.status(400).send(err);
+                if(!UserAuthed) {
+                    res.status(400).send(UserAuthed);
+                }else {
+                console.log(UserAuthed);
+                res.send(UserAuthed.token)
+                }
 
+            })
 
+        }catch (e) {
+            res.status(400).send("user")
+        }
+    })
 }

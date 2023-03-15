87506ed10a534bc568b84f3bf9b67d05d8d24cdb
Arshiya <arshiyah52@gmail.com>
2023-03-15 03:45:11 +0330

    check token collection for token

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,5 +5,23 @@ module.exports = auth = async (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) return res.status(400).send('access denied');
 
+    try {
+        const verified = jwt.verify(token, "MySecretKey");
+        if (!verified) {
+            res.status(400).send("invalid token");
+        }
 
+        const userAuth = await Token.findOne(
+            {
+                _userId: verified.id,
+                token: token,
+                tokenType: 'login'
+            }
+        ).exec();
+        if (!userAuth) {
+            res.status(400).send("database token not found");
+        }
+    }catch(e){
+        res.status(400).send("invalid token");
+    }
 }

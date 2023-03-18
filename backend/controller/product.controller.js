1c2e7e920d6d0da4b5e8b7709122daccf62093d6
Arshiya <arshiyah52@gmail.com>
2023-03-19 03:10:46 +0330

    update product added

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,17 +1,61 @@
+const product = require('../models/product');
 
-
-module.exports.getAllProducts = () => {
-}
-
-module.exports.getOneProduct = () => {
+module.exports.getAllProducts = (req, res) => {
 
 }
-module.exports.createOneProduct = () => {
+
+module.exports.getOneProduct = (req, res) => {
 
 }
-module.exports.updateOneProduct = () => {
+
+module.exports.createOneProduct = (req, res) => {
 
 }
-module.exports.deleteOneProduct = () => {
+module.exports.updateOneProduct = async (req, res) => {
+    const productId = req.params.id;
+    const updates = Object.key(req.body);
+    const allowedUpdate = ['name', 'description', 'category', 'weight', 'picture', 'price']
+    const validation = updates.every((query)=>{
+        allowedUpdate.includes(query)
+    })
+    if(!validation){
+        res.status(400).json({
+            status: false,
+            data: undefined,
+            message: "validation failed"
+        })
+    }
+
+    try {
+        const products = await product.findOne({
+            _id: productId
+        });
+        if (!products){
+            res.status(400).json({
+                status:false,
+                data: undefined,
+                message: "product not found"
+            })
+        }
+
+        updates.forEach((key)=>{
+            products[key] = updates[key];
+        })
+        await updates.save();
+        res.status(400).json({
+            status:true,
+            data: updates,
+            message: "product updated successfully"
+        })
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data : e,
+            message: "error updating product"
+        })
+    }
+}
+
+module.exports.deleteOneProduct = async (req, res) => {
 
 }

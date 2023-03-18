d044d0886da002acf21040efa758f65f31551632
Arshiya <arshiyah52@gmail.com>
2023-03-19 03:11:36 +0330

    get all product added

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,7 +1,27 @@
 const product = require('../models/product');
 
 module.exports.getAllProducts = (req, res) => {
-
+    try {
+        const allProducts = product.findOne({});
+        if (!allProducts){
+            res.status(400).json({
+                status: false,
+                data: undefined,
+                message : 'Product not found'
+            })
+        }
+        res.status(200).json({
+            status: true,
+            data: allProducts,
+            message: 'Product sent successfully'
+        })
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data: e,
+            message: "error getting product"
+        })
+    }
 }
 
 module.exports.getOneProduct = (req, res) => {

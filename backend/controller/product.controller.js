20988b7fc935043987037cff1705ac20a98f74f7
Arshiya <arshiyah52@gmail.com>
2023-03-19 04:18:14 +0330

    merge conflict resolved

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,8 +1,6 @@
-const product_model =require(".././models/product");
+const product =require(".././models/product");
 
-module.exports.getAllProducts = () => {};
 
-module.exports.getOneProduct = () => {};
 module.exports.createOneProduct = async (req, res) => {
   try {
     let payload = {
@@ -15,12 +13,12 @@ module.exports.createOneProduct = async (req, res) => {
       picture: req.body.picture,
     };
     //create product in detabase
-    let product = await product_model.createOneProduct({
+    let products = await product.createOneProduct({
       ...payload,
     });
     res.status(200).json({
       status: true,
-      data: product,
+      data: products,
     });
   } catch (err) {
     console.log(err);
@@ -30,5 +28,126 @@ module.exports.createOneProduct = async (req, res) => {
     });
   }
 };
-module.exports.updateOneProduct = () => {};
-module.exports.deleteOneProduct = () => {};
+
+module.exports.getAllProducts = (req, res) => {
+    try {
+        const allProducts = product.findOne({});
+        if (!allProducts){
+            res.status(400).json({
+                status: false,
+                data: undefined,
+                message : 'Product not found'
+            })
+        }
+        res.status(200).json({
+            status: true,
+            data: allProducts,
+            message: 'Product sent successfully'
+        })
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data: e,
+            message: "error getting product"
+        })
+    }
+}
+
+module.exports.getOneProduct = (req, res) => {
+    const productId = req.params.id;
+    try{
+        const findProduct = product.findOne({
+            _id: productId
+        }).exec();
+        if(!findProduct){
+            res.status(404).json({
+                status: false,
+                data: undefined,
+                message: "Product not found"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            data: findProduct,
+            message: "Product found"
+        })
+
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data: e,
+            message: "error get a product"
+        })
+    }
+}
+
+
+module.exports.updateOneProduct = async (req, res) => {
+    const productId = req.params.id;
+    const updates = Object.key(req.body);
+    const allowedUpdate = ['name', 'description', 'category', 'weight', 'picture', 'price']
+    const validation = updates.every((query)=>{
+        allowedUpdate.includes(query)
+    })
+    if(!validation){
+        res.status(400).json({
+            status: false,
+            data: undefined,
+            message: "validation failed"
+        })
+    }
+
+    try {
+        const products = await product.findOne({
+            _id: productId
+        });
+        if (!products){
+            res.status(400).json({
+                status:false,
+                data: undefined,
+                message: "product not found"
+            })
+        }
+
+        updates.forEach((key)=>{
+            products[key] = updates[key];
+        })
+        await updates.save();
+        res.status(400).json({
+            status:true,
+            data: updates,
+            message: "product updated successfully"
+        })
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data : e,
+            message: "error updating product"
+        })
+    }
+}
+
+module.exports.deleteOneProduct = async (req, res) => {
+    const productId = req.params.id;
+    try{
+        const deleteProductRequest = await product.findByIdAndDelete(productId);
+        if(!deleteProductRequest){
+            res.status(400).json({
+                status: false,
+                data: undefined,
+                message: "could not delete product"
+            })
+        }
+        res.status(200).json({
+            status: true,
+            data: undefined,
+            message: "product deleted successfully"
+        })
+    }catch (e) {
+        res.status(400).json({
+            status: false,
+            data: e,
+            message: "error deleting product"
+        })
+    }
+}

b39d81ffee53ee315cc49b906bfcba5c013a213b
elliot sad <bamdadmahdi05@gmail.com>
2023-03-19 01:13:49 +0100

    creat-product

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,17 +1,34 @@
+const product_model =require(".././models/product");
 
+module.exports.getAllProducts = () => {};
 
-module.exports.getAllProducts = () => {
-}
-
-module.exports.getOneProduct = () => {
-
-}
-module.exports.createOneProduct = () => {
-
-}
-module.exports.updateOneProduct = () => {
-
-}
-module.exports.deleteOneProduct = () => {
-
-}
+module.exports.getOneProduct = () => {};
+module.exports.createOneProduct = async (req, res) => {
+  try {
+    let payload = {
+      name: req.body.name,
+      seller: req.userId,
+      description: req.body.description,
+      category: req.body.category,
+      weight: req.body.weight,
+      price: req.body.price,
+      picture: req.body.picture,
+    };
+    //create product in detabase
+    let product = await product_model.createOneProduct({
+      ...payload,
+    });
+    res.status(200).json({
+      status: true,
+      data: product,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err,
+      status: false,
+    });
+  }
+};
+module.exports.updateOneProduct = () => {};
+module.exports.deleteOneProduct = () => {};

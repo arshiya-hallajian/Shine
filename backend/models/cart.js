49d3eded6e84fcf6f9ff6abde72af88352f06ba1
elliot sad <bamdadmahdi05@gmail.com>
2023-03-28 02:32:33 +0200

    cart model added

---
+++ backend/models/cart.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+//
+const cartSchema = new mongoose.Schema({
+    sellerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+    items: [{
+        itemId:{
+            type: mongoose.Schema.Type.ObjectId,
+            required: true,
+            ref: 'product'
+        },
+    name: {
+        type: String,
+        required: true
+    },
+
+    quantity : {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: Number
+    
+}],
+checkout: {
+    type: Number,
+    required: true,
+    default: 0
+}
+});
+
+const cart_model = mongoose.model('cart', cartSchema);
+
+module.exports = cart_model;
+

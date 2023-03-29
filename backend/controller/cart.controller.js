374b72ac710a215c9e754daa5e885e802b0fa4a9
elliot sad <bamdadmahdi05@gmail.com>
2023-03-29 04:52:02 +0200

    clean better code

--- backend/controller/cart.controller.js
+++ backend/controller/cart.controller.js
@@ -1,40 +1,32 @@
-const Cart = require('../models/cart');
-const Product = require('../models/product');
+const Cart = require("../models/cart");
+const Product = require("../models/product");
 
 // Select a product from the shopping cart
 module.exports.getCart = async (req, res) => {
-    
-    const sellerId = req.userId
-    
-    try{    
-        const cart = await Cart.findOne({sellerId});
+  const sellerId = req.userId;
 
-         if(cart && cart.items.length > 0){
-            res.status(200).json({
-                status: true, 
-                data: cart
-            });
-         }else{
-            res.status(400).json({
-                status : false,
-                data : null
-            });
-         }
-        
-    }catch(e){
-        res.status(400).json({
-            status:false,
-            data: e
-        })
+  try {
+    const cart = await Cart.findOne({ sellerId });
+
+    if (cart && cart.items.length > 0) {
+      res.status(200).json({
+        status: true,
+        data: cart,
+      });
+    } else {
+      res.status(400).json({
+        status: false,
+        data: null,
+      });
     }
-}
+  } catch (e) {
+    res.status(400).json({
+      status: false,
+      data: e,
+    });
+  }
+};
 
+module.exports.addToCart = (req, res) => {};
 
-module.exports.addToCart = (req, res) => {
-    
-}
-
-
-module.exports.removeFromCart = (req, res) => {
-     
-}
+module.exports.removeFromCart = (req, res) => {};

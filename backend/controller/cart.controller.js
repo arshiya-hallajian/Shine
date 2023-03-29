327b8bb20b36f2fad47f2eccab429167c0ef47f3
elliot sad <bamdadmahdi05@gmail.com>
2023-03-29 04:21:11 +0200

    Select-a-product from-the-shopping-cart

--- backend/controller/cart.controller.js
+++ backend/controller/cart.controller.js
@@ -1,9 +1,32 @@
-const cart = require('../models/cart');
+const Cart = require('../models/cart');
+const Product = require('../models/product');
 
+// Select a product from the shopping cart
+module.exports.getCart = async (req, res) => {
+    
+    const sellerId = req.userId
+    
+    try{    
+        const cart = await Cart.findOne({sellerId});
 
-
-module.exports.getCart = (req, res) => {
-
+         if(cart && cart.items.length > 0){
+            res.status(200).json({
+                status: true, 
+                data: cart
+            });
+         }else{
+            res.status(400).json({
+                status : false,
+                data : null
+            });
+         }
+        
+    }catch(e){
+        res.status(400).json({
+            status:false,
+            data: e
+        })
+    }
 }
 
 
@@ -13,5 +36,5 @@ module.exports.addToCart = (req, res) => {
 
 
 module.exports.removeFromCart = (req, res) => {
-    
+     
 }

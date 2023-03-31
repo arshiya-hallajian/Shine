616ad9ac4d5ab76abec29565e7af72631860cefd
elliot sad <bamdadmahdi05@gmail.com>
2023-04-01 01:01:40 +0200

    added remove from cart function

--- backend/controller/cart.controller.js
+++ backend/controller/cart.controller.js
@@ -29,4 +29,47 @@ module.exports.getCart = async (req, res) => {
 
 module.exports.addToCart = (req, res) => {};
 
-module.exports.removeFromCart = (req, res) => {};
+//remove to cart function
+module.exports.removeFromCart = async (req, res) => {
+  const sellerId = req.userId;
+  const itemId = req.params.itemsId;
+
+  try {
+    // find cart in data base
+    const cart = await Cart.findOne({ sellerId: sellerId });
+
+    const findIndex = cart.items.findIndex((item) => item.itemId === itemId);
+
+    //if product doesn't exists
+    if (findIndex == -1) {
+      res.status(404).json({
+        status: false,
+        data: null,
+        message: "item not found",
+      });
+    } else {
+      const item = cart.items[findIndex]; 
+
+      // pirce bill minos item multiple quantity      
+      cart.checkout =
+        cart.checkout -
+        cart.items[findIndex].price * cart.items[findIndex].quantity;
+
+        // remove products from products array
+      cart.items.splice(findIndex, 1);
+      await cart.save();
+
+      res.status(200).json({
+        status: true,
+        data: null,
+      message: "item removed"
+      });
+    }
+  } catch (err) {
+    res.status(404).json({
+      status: false,
+      data: err,
+      message: "error in db happend"
+    });
+  }
+};

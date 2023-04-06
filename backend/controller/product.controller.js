40be4c1d97d0ab03179a360aa6cc324a07b77a82
Arshiya <arshiyah52@gmail.com>
2023-04-07 03:11:25 +0330

    Update product.controller.js

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -22,7 +22,13 @@ module.exports.createOneProduct = async (req, res) => {
         inSale_Price: req.body.inSale_Price,
         picture: req.body.picture,
     });
-  } catch (err) {
+  try {
+        await products.save();
+        res.status(200).json({
+            status: true,
+            data: products,
+        });
+    } catch (err) {
     console.log(err);
     res.status(500).json({
       error: err,

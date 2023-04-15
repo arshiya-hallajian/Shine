b04fde26f01716e62c932815a3c58d982c202f25
Arta Yqubi <arta.dev21@gmail.com>
2023-04-15 10:28:55 +0330

    basic blog-contents

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,16 +2,16 @@ import React, { useRef, useState } from "react";
 import './Home.css'
 import WhyThisShop from '../../components/whyThisShop/WhyThisShop'
 import HomeIntroProducts from '../../components/HomeIntroProducts/HomeIntroProducts'
-import blogs from "../../components/blogs/Blogs"
 import OnSaleCart from '../../components/OnSaleCart/OnSaleCart'
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { RxPinTop } from "react-icons/rx"
+import Blogs from "../../components/blogs/Blogs"
+
 
 export default function Home() {
   window.addEventListener('scroll', function () {
@@ -139,14 +139,19 @@ export default function Home() {
       </a>
 
       {/* Blogs */} 
+
     <hr className="hr-line"/>
       <div className="blogs-content">
 
         <h4>مطالب وبلاگ</h4>
         <p>Blog content</p>
 
-        <section className="blog-card">
+        <section className="blog_card">
 
+        <Blogs image="https://dev-wp.ir/goldpluss/wp-content/uploads/2021/12/cover04-min.jpg" title ="قصد هدیه گرفتن دارید ؟" parag ="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است..." />
+        <Blogs image="https://dev-wp.ir/goldpluss/wp-content/uploads/2021/12/1-5-min.jpg" title ="قصد هدیه گرفتن دارید ؟" parag ="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است..." />
+        <Blogs image="https://dev-wp.ir/goldpluss/wp-content/uploads/2021/12/2-8-min.jpg" title ="قصد هدیه گرفتن دارید ؟" parag ="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است..." />
+      
         </section>
       </div>
 

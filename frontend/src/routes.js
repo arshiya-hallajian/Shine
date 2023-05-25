fcc0a7dcf60b30e59fbf224f199701db83e6038c
KasraMg <kasragithub7@gmail.com>
2023-05-25 17:33:45 +0330

    update

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,8 +2,9 @@ import Home from "./pages/Home/Home"
 import PageNotFound from "./pages/404/404"
 import ProductPage from './pages/ProductPage/ProductPage'
 import Category from "./pages/Category/Category"
-import Login from './pages/login-signup/Login'
+import Login from './pages/Login/Login'
 import SignUp from "./pages/SignUp/SignUp"
+ 
 const routes = [
   {
     path: '/', element: <Home />

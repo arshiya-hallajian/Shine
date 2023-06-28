ac7bd373a765d328092019cab28c8872e345277b
Arta Yqubi <arta.dev21@gmail.com>
2023-06-28 11:57:36 +0330

    Optimizing ...

--- frontend/src/components/ProductPage-header-general/ProductPage_general.jsx
+++ frontend/src/components/ProductPage-header-general/ProductPage_general.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './ProductPage_general.css';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { BsSearch, BsPerson } from 'react-icons/bs';
-import { AiOutlineShoppingCart, AiOutlineHeart, AiOutlineMenu,AiOutlineTeam,AiOutlinePhone } from 'react-icons/ai';
-import {BiSearch,BiHomeAlt,BiStore,BiGitCompare,BiLogIn} from 'react-icons/bi';
+import { AiOutlineShoppingCart, AiOutlineHeart, AiOutlineMenu, AiOutlineTeam, AiOutlinePhone } from 'react-icons/ai';
+import { BiSearch, BiHomeAlt, BiStore, BiGitCompare, BiLogIn } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 export default function ProductPage_general() {
 
@@ -50,28 +50,28 @@ export default function ProductPage_general() {
               <div className='PP_general_canvas_searchbar'>
                 <form action="">
                   <input type="text" placeholder='جستجو محصولات' />
-                  <button className='canvas_searchbar_submit'><BiSearch/></button>
+                  <button className='canvas_searchbar_submit'><BiSearch /></button>
                 </form>
               </div>
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-        <div className='canvas_body'>
-      <ul className='canvas_body_items'>
+            <div className='canvas_body'>
+              <ul className='canvas_body_items'>
 
-        <li><span><BiHomeAlt /></span> <Link to="/">خانه اصلی</Link></li>
-        <li><span><BiStore /></span><Link to="/Category"> فروشگاه</Link></li>
-        <li> <span><AiOutlineTeam /></span><Link to="/ContactUs">درباره ما</Link> </li>
-        <li><span><BiGitCompare /></span><Link to="/">مورد علاقه ها</Link> </li>
-        <li className='border-0'><span><BiLogIn /></span><Link to="/Login">ورود | ثبت نام</Link> </li>
+                <li><span><BiHomeAlt /></span> <Link to="/">خانه اصلی</Link></li>
+                <li><span><BiStore /></span><Link to="/Category"> فروشگاه</Link></li>
+                <li> <span><AiOutlineTeam /></span><Link to="/ContactUs">درباره ما</Link> </li>
+                <li><span><BiGitCompare /></span><Link to="/">مورد علاقه ها</Link> </li>
+                <li className='border-0'><span><BiLogIn /></span><Link to="/Login">ورود | ثبت نام</Link> </li>
 
 
-      </ul>
+              </ul>
 
-      <section>
-        <p>Shine Like A Diamond</p>
-      </section>
-        </div>
+              <section>
+                <p>Shine Like A Diamond</p>
+              </section>
+            </div>
           </Offcanvas.Body>
         </Offcanvas>
       </div>
